73be77bd6b11e64a04ebdc8968998210c9b6b8b0
SpaceNerd0717 <58666416+Void0717@users.noreply.github.com>
2023-10-25 08:52:02 -0400

    api call test 2

--- apicall.js
+++ apicall.js
@@ -9,10 +9,30 @@ fetch(apiUrl)
         const temperatureElement = document.getElementById('temperature');
         const conditionElement = document.getElementById('condition');
 
-        locationElement.textContent = data.name;
-        temperatureElement.textContent = data.main.temp;
-        conditionElement.textContent = data.weather[0].description;
+        // Access the relevant weather data
+        const timeline = data.data.timelines[0];
+        const temperature = timeline.intervals[0].values.temperature_2m;
+        const weatherCode = timeline.intervals[0].values.weathercode;
+
+        locationElement.textContent = location;
+        temperatureElement.textContent = temperature + '°C';
+        conditionElement.textContent = getWeatherCondition(weatherCode);
     })
     .catch((error) => {
         console.error('Error fetching weather data: ' + error);
     });
+
+function getWeatherCondition(weatherCode) {
+    // Define a function to map Tomorrow.io weather codes to human-readable conditions
+    switch (weatherCode) {
+        case 0:
+            return 'Clear';
+        case 1:
+            return 'Partly Cloudy';
+        case 2:
+            return 'Cloudy';
+        // Add more cases as needed
+        default:
+            return 'Unknown';
+    }
+}
