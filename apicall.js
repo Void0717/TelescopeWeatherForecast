ea1e76fd48774c7bd234618b5e413618981317c3
SpaceNerd0717 <58666416+Void0717@users.noreply.github.com>
2023-10-27 08:30:48 -0400

    test

--- apicall.js
+++ apicall.js
@@ -1,38 +1,79 @@
-const apiKey = 'YOUR_API_KEY';
-const location = 'Dumfries, US'; // E.g., 'New York, US'
-const apiUrl = 'https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=T4X7K5unTCkpTAmf4BW2mtXJs3xSOJXo';
-
-fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-        const locationElement = document.getElementById('location');
-        const temperatureElement = document.getElementById('temperature');
-        const conditionElement = document.getElementById('condition');
-
-        // Access the relevant weather data
-        const timeline = data.data.timelines[0];
-        const temperature = timeline.intervals[0].values.temperature_2m;
-        const weatherCode = timeline.intervals[0].values.weathercode;
-
-        locationElement.textContent = location;
-        temperatureElement.textContent = temperature + '°C';
-        conditionElement.textContent = getWeatherCondition(weatherCode);
-    })
-    .catch((error) => {
-        console.error('Error fetching weather data: ' + error);
+const wrapper = document.querySelector(".wrapper"),
+inputPart = document.querySelector(".input-part"),
+infoTxt = inputPart.querySelector(".info-txt"),
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+weatherPart = wrapper.querySelector(".weather-part"),
+wIcon = weatherPart.querySelector("img"),
+arrowBack = wrapper.querySelector("header i");
+let api;
+inputField.addEventListener("keyup", e =>{
+    if(e.key == "Enter" && inputField.value != ""){
+        requestApi(inputField.value);
+    }
+});
+locationBtn.addEventListener("click", () =>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        alert("Your browser not support geolocation api");
+    }
+});
+function requestApi(city){
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
+    fetchData();
+}
+function onSuccess(position){
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    fetchData();
+}
+function onError(error){
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add("error");
+}
+function fetchData(){
+    infoTxt.innerText = "Getting weather details...";
+    infoTxt.classList.add("pending");
+    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
+        infoTxt.innerText = "Something went wrong";
+        infoTxt.classList.replace("pending", "error");
     });
-
-function getWeatherCondition(weatherCode) {
-    // Define a function to map Tomorrow.io weather codes to human-readable conditions
-    switch (weatherCode) {
-        case 0:
-            return 'Clear';
-        case 1:
-            return 'Partly Cloudy';
-        case 2:
-            return 'Cloudy';
-        // Add more cases as needed
-        default:
-            return 'Unknown';
+}
+function weatherDetails(info){
+    if(info.cod == "404"){
+        infoTxt.classList.replace("pending", "error");
+        infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+    }else{
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {temp, feels_like, humidity} = info.main;
+        if(id == 800){
+            wIcon.src = "icons/clear.svg";
+        }else if(id >= 200 && id <= 232){
+            wIcon.src = "icons/storm.svg";  
+        }else if(id >= 600 && id <= 622){
+            wIcon.src = "icons/snow.svg";
+        }else if(id >= 701 && id <= 781){
+            wIcon.src = "icons/haze.svg";
+        }else if(id >= 801 && id <= 804){
+            wIcon.src = "icons/cloud.svg";
+        }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
+            wIcon.src = "icons/rain.svg";
+        }
+        
+        weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+        weatherPart.querySelector(".weather").innerText = description;
+        weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+        weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+        weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
+        infoTxt.classList.remove("pending", "error");
+        infoTxt.innerText = "";
+        inputField.value = "";
+        wrapper.classList.add("active");
     }
 }
+arrowBack.addEventListener("click", ()=>{
+    wrapper.classList.remove("active");
+});
